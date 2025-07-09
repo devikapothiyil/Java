73a10c10a5fd6a561baaff31c2930e9aedb544dd
Devika Rajeev <145443027+devikapothiyil@users.noreply.github.com>
2025-07-09 06:37:08 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,11 @@
+public class Factorial {
+    static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial: " + factorial(num));
+    }
+}
